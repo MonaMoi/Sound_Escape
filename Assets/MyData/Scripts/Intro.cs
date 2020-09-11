90a434920b22394c38917ea34b9c72f8502e957a
marquitoh <markus.haberl95@googlemail.com>
2020-09-11 22:38:23 +0200

    Marquito Work 2.0
    Intro - Sound + Animation und Szenenwechsel.
    Aufzug verändert.

---
+++ Assets/MyData/Scripts/Intro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("LoadNextScene");
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(26.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
