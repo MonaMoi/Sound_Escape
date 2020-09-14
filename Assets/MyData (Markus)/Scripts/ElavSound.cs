024b51609efa71db6dbf638349a09c71dc55394d
marquitoh <markus.haberl95@googlemail.com>
2020-09-14 22:45:41 +0200

    Shaft Sound Trigger + Drugged Zone + Szenenwechsel

--- Assets/MyData (Markus)/Scripts/ElavSound.cs
+++ Assets/MyData (Markus)/Scripts/ElavSound.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ElavSound : MonoBehaviour
 {
     public AudioSource Source;
-    public AudioSource Source2;
 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
@@ -18,7 +17,5 @@ public class ElavSound : MonoBehaviour
     {
         yield return new WaitForSeconds(5.0f);
         Source.Play();
-        yield return new WaitForSeconds(3.0f);
-        Source2.Play();
     }
 }
---
+++ Assets/MyData (Markus)/Scripts/ShaftPlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ShaftPlay : MonoBehaviour
+{
+    public Animator shaft;
+    
+    private void OnTriggerStay()
+    {
+        StartCoroutine(LoadShaftAnimation());
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    private IEnumerator LoadShaftAnimation()
+    {
+        yield return new WaitForSeconds(5f);
+        shaft.SetTrigger("Start");
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+
+        yield return new WaitForSeconds(30f);
+         SceneManager.LoadScene(levelIndex);
+    }
+}
