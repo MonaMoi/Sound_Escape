868ba13baf3a4881765e008b1d0470ecc357222b
mvassili <34132904+mvassili@users.noreply.github.com>
2020-09-14 16:03:34 +0200

    dfg

---
+++ Assets/Scripts/DopplerLightMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DopplerLightMovement : MonoBehaviour
+{
+    private bool dirRight = true;
+     public float speed = 2.0f;
+ 
+     void Update () {
+         if (dirRight)
+             transform.Translate (Vector2.right * speed * Time.deltaTime);
+         else
+             transform.Translate (-Vector2.right * speed * Time.deltaTime);
+         
+         if(transform.position.x >= 53.27) {
+             dirRight = false;
+         }
+         
+         if(transform.position.x <=- 13.63f) {
+             dirRight = true;
+         }
+     }
+}
--- Assets/Scripts/WaldSkripte/TriggerAudio.cs
+++ Assets/Scripts/WaldSkripte/TriggerAudio.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class TriggerAudio : MonoBehaviour
 {
-    public GameObject AudioSource; //reference to object that will get a new audio
+    public GameObject PlayerSource; //reference to audio of the narrator
+    
+    public GameObject AudioSourceToChange; //reference to object that will get a new audio
     public GameObject headphones; //headphones to be activated
     public GameObject Kopfhoerer; //headphones to be destroyed
     public AudioClip newAudio; //<---drag mp3 into the inspector here
+
+    public AudioClip nextNarrator;
     AudioSource audio;
+
+    AudioSource narratorAudio;
 
     // Start is called before the first frame update
     void Start()
     {
-        audio=AudioSource.GetComponent<AudioSource>(); //access Prefab audio
+        audio=PlayerSource.GetComponent<AudioSource>(); //access Prefab audio
+
+        narratorAudio=AudioSourceToChange.GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter()
     {
         headphones.SetActive(true);
 
-        audio.clip = newAudio;
+        audio.clip = nextNarrator;
+        audio.Play();
+
+        narratorAudio.clip = newAudio;
         audio.Play();
 
         Destroy(Kopfhoerer);
