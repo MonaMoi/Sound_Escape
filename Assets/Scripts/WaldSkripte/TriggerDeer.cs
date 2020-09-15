4e803ec4b9441f24bd2cc30fb580a8a316543e6c
mvassili <34132904+mvassili@users.noreply.github.com>
2020-09-15 21:26:20 +0200

    Sounds Wald + Reh

---
+++ Assets/Scripts/WaldSkripte/TriggerDeer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDeer : MonoBehaviour
+{
+    public GameObject PlayerSource; //reference to audio of the narrator
+    
+    public GameObject headphones; //headphones to be activated
+    public GameObject Kopfhoerer; //headphones to be destroyed
+
+    public AudioClip nextNarrator;
+
+   AudioSource Audio;
+
+    public GameObject oldDeer;
+   public GameObject newDeer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Audio=PlayerSource.GetComponent<AudioSource>(); //access Prefab audio
+
+    }
+
+    void OnTriggerEnter()
+    {
+        headphones.SetActive(true);
+
+        Audio.clip = nextNarrator;
+        Audio.Play();
+
+        Destroy(oldDeer);
+        newDeer.SetActive(true);
+
+        Destroy(Kopfhoerer);
+    }
+
+}
---
+++ Assets/Scripts/WaldSkripte/triggerReh.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerReh : MonoBehaviour
+{
+    public GameObject SpeakingReh;
+   void OnTriggerEnter() {
+
+       SpeakingReh.SetActive(true);
+
+   }
+}
