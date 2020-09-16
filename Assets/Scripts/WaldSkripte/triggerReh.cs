9e81a6471ea9e9af4b84af9627d0ccc8e55b492d
MonaMoi <35458275+MonaMoi@users.noreply.github.com>
2020-09-16 13:29:36 +0200

    Linux Settings
    Einstellungen update

--- Assets/Scripts/WaldSkripte/triggerReh.cs
+++ Assets/Scripts/WaldSkripte/triggerReh.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class triggerReh : MonoBehaviour
 {
     public GameObject SpeakingReh;
+    public GameObject Trigger;
+
    void OnTriggerEnter() {
 
        SpeakingReh.SetActive(true);
+       Destroy(Trigger);
 
    }
 }
